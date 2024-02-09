4a3af60e5fb0af4f7dbd70bee2379479fe3684ba
THEVENARD Julien <thevenard.julien@gmail.com>
2024-02-09 04:45:24 +0100

    config localhost prod

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,4 @@
-const localhost = "http://localhost:1337";
+// const localhost = "http://localhost:1337"
+const localhost = "https://oc-jt-p12-my-portfolio-build-back.vercel.app/";
 
 export { localhost };
--- src/services/postsAPI.tsx
+++ src/services/postsAPI.tsx
@@ -21,44 +21,4 @@ const findPosts = async () => {
   }
 };
 
-const addFile = async (file) => {
-  const token = localStorage.getItem("token") || null;
-  try {
-    const response = await fetch(`${localhost}/api/upload/`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: file,
-    });
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP! Statut : ${response.status}`);
-    }
-  } catch (error) {
-    console.error("Erreur lors de l'envoi du post :", error);
-  }
-};
-
-const addPostWithFile = async (formData) => {
-  const token = localStorage.getItem("token") || null;
-  try {
-    const response = await fetch(`${localhost}/api/posts`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        Title: formData.Title,
-        Category: formData.Category,
-        Description: formData.Description,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP! Statut : ${response.status}`);
-    }
-  } catch (error) {
-    console.error("Erreur lors de l'envoi du post :", error);
-  }
-};
-
-export { addFile, addPostWithFile, findPosts };
+export { findPosts };

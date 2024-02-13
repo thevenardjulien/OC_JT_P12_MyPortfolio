f644267e7e72eb67bb1e6d3254bc5248d75ff0f2
THEVENARD Julien <thevenard.julien@gmail.com>
2024-02-13 19:49:47 +0100

    hero img bg dir

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,5 @@
 const localhost = "http://localhost:5173";
-const localhostBack = "http://localhost:3000/";
+const localhostBack =
+  "https://p12backend-43fy1so2z-thevenardjuliens-projects.vercel.app/";
 
 export { localhost, localhostBack };

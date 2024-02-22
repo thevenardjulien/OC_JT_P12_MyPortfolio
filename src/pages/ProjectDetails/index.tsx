b226768c5ecd30f282af766ffbe4aaa0d84bfbb7
THEVENARD Julien <thevenard.julien@gmail.com>
2024-02-22 21:43:09 +0100

    Projects proposals

--- src/pages/ProjectDetails/index.tsx
+++ src/pages/ProjectDetails/index.tsx
@@ -1,4 +1,3 @@
-import { Link, useParams } from "react-router-dom";
 import Footer from "../../layout/Footer";
 import Header from "../../layout/Header";
 import "./style.scss";
@@ -9,15 +8,26 @@ import Loader from "../../components/Loader";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const ProjectDetails = () => {
   const [post, setPost] = useState(undefined);
+  const [proposals, setProposals] = useState([]);
+  const navigate = useNavigate();
   const params = useParams();
 
-  const getPost = async () => {
+  const getPosts = async () => {
     try {
       const projects = await getAllProjects();
-      setPost(projects.filter((project) => project.title === params.title));
+      setProposals(
+        projects
+          .slice()
+          .reverse()
+          .filter((project) => project.title !== params.title)
+      );
+      setPost(
+        projects.slice().filter((project) => project.title === params.title)
+      );
     } catch (error) {
       toast.error("Unable to retrieve project list, try again later...");
       console.error(
@@ -28,13 +38,19 @@ const ProjectDetails = () => {
   };
 
   useEffect(() => {
-    getPost();
+    getPosts();
   }, [params.title]);
 
   const createBlobUrl = (data: { data: number[] }, contentType: string) => {
     const uint8Array = new Uint8Array(data.data);
     const blob = new Blob([uint8Array], { type: contentType });
     return URL.createObjectURL(blob);
+  };
+
+  const handleClick = ({ currentTarget }) => {
+    const newParams = currentTarget.children[0].alt;
+    navigate(`/project-details/${newParams}`);
+    window.scrollTo(0, 0);
   };
 
   return (
@@ -57,31 +73,65 @@ const ProjectDetails = () => {
                 )}
               </div>
               <div className="postWrapper__infos">
-                <div className="postWrapper__category">{post[0].category}</div>
-                <div className="postWrapper__title">{post[0].title}</div>
+                <div className="postWrapper__tagTitleWrapper">
+                  <div className="postWrapper__category">
+                    {post[0].category}
+                  </div>
+                  <div className="postWrapper__title">{post[0].title}</div>
+                </div>
+
                 <div className="postWrapper__description">
                   {post[0].description}
                 </div>
-                {post[0].github && (
-                  <div className="postWrapper__github">
-                    <Link
-                      className="Infos__link"
-                      to={post[0].github}
-                      target="_blank"
+                <div className="postWrapper__links">
+                  {post[0].github && (
+                    <div className="postWrapper__github">
+                      <Link
+                        className="Infos__link"
+                        to={post[0].github}
+                        target="_blank"
+                      >
+                        <FontAwesomeIcon icon={faGithub} /> View on Github
+                      </Link>
+                    </div>
+                  )}
+                  {post[0].lien && (
+                    <div className="postWrapper__link">
+                      <Link
+                        className="Infos__link"
+                        to={post[0].lien}
+                        target="_blank"
+                      >
+                        <FontAwesomeIcon icon={faLink} /> Direct Link
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="postWrapper__proposals">
+                You can also check out my other projects:
+                {proposals &&
+                  proposals.map((proposal, index) => (
+                    <div
+                      key={index}
+                      onClick={(e) => handleClick(e)}
+                      className="proposalWrapper"
                     >
-                      <FontAwesomeIcon icon={faGithub} /> View on Github
-                    </Link>
-                  </div>
-                )}
-                {post[0].lien && (
-                  <Link
-                    className="Infos__link"
-                    to={post[0].lien}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faLink} /> Direct Link
-                  </Link>
-                )}
+                      {proposal.images && proposal.images.length > 0 && (
+                        <img
+                          className="postWrapper__proposalsImg"
+                          src={createBlobUrl(
+                            proposal.images[0].data,
+                            proposal.images[0].contentType
+                          )}
+                          alt={proposal.title}
+                        />
+                      )}
+                      <span className="proposalWrapper__text">
+                        {proposal.title}
+                      </span>
+                    </div>
+                  ))}
               </div>
             </div>
           </>
